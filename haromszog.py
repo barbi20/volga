66bdd094a41f1ac0d8cefe59d53146fa73553819
barbi20 <hellerbarbi012@gmail.com>
2023-05-04 14:07:19 +0200

    kesz

--- haromszog.py
+++ haromszog.py
@@ -1,4 +1,9 @@
 #haromszog
-a=int(input"add meg a haromszog 'a' oldalat: "))
-b=int(input"add meg a haromszog 'b' oldalat: "))
-c=int(input"add meg a haromszog 'c' oldalat: "))
+a=int(input("add meg a haromszog 'a' oldalat: "))
+b=int(input("add meg a haromszog 'b' oldalat: "))
+c=int(input("add meg a haromszog 'c' oldalat: "))
+
+if c<a+b and a<b+c and b<a+c:
+	print("A(z), a,",",b,","és",c,"haromszoget alkot")
+else:
+	print("A(z), a,",",b,","és",c,"nem alkot haromszoget")
